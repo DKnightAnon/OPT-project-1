39c0108e3c51e4113d79dfa58ca8f90e0f76882e
DKnightAnon <a.r.c.delgado@outlook.com>
2023-03-27 13:56:19 +0200

    Screenshot van UML4 toegevoegd. Moet nog verder uitgewerkt worden.)

--- ShipFlexcode/src/Main.java
+++ ShipFlexcode/src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
-<<<<<<< HEAD
-    public static void Main(String args[]){
-        System.out.println("Hi!");
-=======
     public static void main(String[] args) {
         System.out.println("test!");
->>>>>>> origin/main
     }
 }
