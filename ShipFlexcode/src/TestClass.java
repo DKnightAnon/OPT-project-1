a6967ecae56e6fbde63d9512a163b546521a4d63
David van der Harst <david@vanderharst.org>
2023-04-06 09:35:54 +0200

    Objecten appart gedeclereerd per methode, @Test boven methode gezet.

--- ShipFlexcode/src/TestClass.java
+++ ShipFlexcode/src/TestClass.java
@@ -1,35 +1,33 @@
+import org.junit.jupiter.api.Test;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestClass {
-    OptieLijst optielijstTest;
-    Offerte offerteTest;
-    Scanner scanner;
+    @Test
     public void testVoegAlleOptiesToeAanLijst() {
-        //@Test
         //Arrange
-        optielijstTest = new OptieLijst();
+        OptieLijst optielijstTest = new OptieLijst();
         //Act
         optielijstTest.voegAlleOptiesToeAanLijst(optielijstTest);
         //Assert
         assertEquals(8, optielijstTest.optielijst.size());
     }
+    @Test
     public void testMaakOptieAan(){
-        //@Test
         //Arrange
-        optielijstTest = new OptieLijst();
-        scanner = new Scanner(System.in);
+        OptieLijst optielijstTest = new OptieLijst();
+        Scanner scanner = new Scanner(System.in);
         String scan = scanner.nextLine();
         //Act
         optielijstTest.maakOptieAan(scanner, scan);
         //Assert
         assertEquals(1, optielijstTest.optielijst.size());
     }
+    @Test
     public void testVoegOptieToe(){
-        //@Test
         //Arrange
-        offerteTest = new Offerte();
-        scanner = new Scanner(System.in);
-        optielijstTest = new OptieLijst();
+        Offerte offerteTest = new Offerte();
+        Scanner scanner = new Scanner(System.in);
+        OptieLijst optielijstTest = new OptieLijst();
         //Act
         offerteTest.voegOptieToe(scanner, "Roer", optielijstTest);
         offerteTest.voegOptieToe(scanner, "Anker", optielijstTest);
